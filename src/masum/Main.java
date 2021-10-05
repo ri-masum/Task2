5256226af4654ade99e155d5cb9ab02e79bc4ad5
ri-masum <87141320+ri-masum@users.noreply.github.com>
2021-10-05 17:09:48 +0600

    Task2 commit

---
+++ src/masum/Main.java
@@ -0,0 +1,28 @@
+/****************************************************
+ * Student Name      :Masum Ahmed                   *
+ * Student ID        :2012020116                    *
+ * Section           :C                             *
+ * Batch             :53                            *
+ * Date              :05-08-21                      *
+ * email             :cse_2012020116@lus.ac.bd      *
+ *                                                  *
+ ****************************************************
+ */
+
+
+package masum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s=new Student();
+      //  Student s2=new Student(2012020116);
+        //Student s3=new Student(2012020116,"Maasum");
+        //University name print
+        s.display();
+
+
+
+    }
+}
---
+++ src/masum/Student.java
@@ -0,0 +1,51 @@
+/****************************************************
+ * Student Name      :Masum Ahmed                   *
+ * Student ID        :2012020116                    *
+ * Section           :C                             *
+ * Batch             :53                            *
+ * Date              :05-08-21                      *
+ * email             :cse_2012020116@lus.ac.bd      *
+ *                                                  *
+ ****************************************************
+ */
+
+
+package masum;
+
+public class Student {
+    //instance variables
+    String name;
+    int id ;
+    //class variable
+    String universityName="Leading University";
+
+   Student()
+   {
+       System.out.println("default constructor");
+   }
+
+   Student(int i)
+   {
+       i=id;
+       System.out.println("Student ID :"+id);
+   }
+
+
+   //constructor overloading
+   Student(int id, String name)
+    {
+        //using this keyword to avoid variable hiding
+        this.id =id;
+        this.name=name;
+        System.out.println("Student name amd  id is : "+name+ " "+ id);
+    }
+
+    void display()
+    {
+
+        System.out.println("University:  "+universityName);
+
+    }
+
+
+}
